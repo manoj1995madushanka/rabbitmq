0d061dfbed8d7b9ad9069d19455d37db120d8b3a
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-09 14:44:12 +0530

    send first message to Queue-1

--- src/main/java/com/example/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/example/rabbitmq/RabbitmqApplication.java
@@ -1,13 +1,30 @@
 package com.example.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class RabbitmqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqApplication.class, args);
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.newConnection();
+		Channel channel = connection.createChannel();
+
+		String message = "First message from RabbitMQ";
+
+		// exchange, routing key, properties, body
+		channel.basicPublish("","Queue-1",null,message.getBytes(StandardCharsets.UTF_8));
+
+		channel.close();
+		connection.close();
 	}
 
 }
